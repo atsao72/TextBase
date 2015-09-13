b8f9e1f7b6fffc3b59ab76cd463860e0fa2a16c5
Riley Chang <chang.riley.z@gmail.com>
2015-09-12 21:49:48 -0400

    users can see actual reviews

--- js/scripts.js
+++ js/scripts.js
@@ -42,16 +42,110 @@ function getBookId(){
     document.getElementById('bookId').value = bookId;
 }
 
-function loadReviews() {
+function downloadBook() {
   var id = parseId();
   var Book = Parse.Object.extend("Book");
   var query = new Parse.Query(Book);
   query.get(id, {
-  success: function(result) {
-  },
-  error: function(error) {
+    success: displayTextbookData,
+    error: function(error) {
+      alert(error);
+    }
+  });
+}
+
+function displayTextbookData(result) {
+  var title = result.get("title"); // string
+  var authors = result.get("authors"); // array
+  var course = result.get("course"); // string
+  var schools = result.get("schools"); // array
+  var reviewsIds = result.get("reviews"); // array
+  var element = document.getElementById("header");
+  var titlediv = document.createElement("div");
+  var node = document.createTextNode(title);
+  titlediv.appendChild(node);
+  element.appendChild(titlediv);
+  var progress = document.createElement("progress");
+  progress.setAttribute("value", "22");
+  progress.setAttribute("max", "100");
+  element.appendChild(progress);
+  var br = document.createElement("br");
+  element.appendChild(br);
+  var percText = document.createElement("div");
+  var node1 = document.createTextNode("of reviewers said the book was necessary for success in the class");
+  element.appendChild(node1);
+/**
+<form method='LINK' action='leaveReview.html' id='reviewButton'/>
+    <input id='bookId1' type='hidden' name='book'></input>
+   <input id='leaveReview' type='submit' value='Review this textbook!' onclick='updateBookId();'></input>
+</form>
+*/
+  var button = document.createElement("form");
+  button.method = "LINK";
+  button.action = "leaveReview.html";
+  button.id = "reviewButton";
+  var input1 = document.createElement("input");
+  input1.id = "bookId1";
+  input1.type = "hidden";
+  input1.name = "book";
+  var input2 = document.createElement("input");
+  input2.id = "leaveReview";
+  input2.type = "submit";
+  input2.value = "Review this textbook!";
+  input2.onclick = "updateBookId();";
+  button.appendChild(input1);
+  button.appendChild(input2);
+  element.appendChild(button);
+
+
+  var Review = Parse.Object.extend("Review");
+  var query = new Parse.Query(Review);
+  for (var i = 0; i < reviewsIds.length; i++) {
+    query.get(reviewsIds[i], {
+      success: showReview,
+      error: function(error) {
+        alert(error);
+      }
+    });
   }
-});
+}
+
+function showReview(review) {
+  var element = document.getElementById("body");
+  var isNess = review.get("isNecessary");
+  if (isNess) {
+    var node = document.createTextNode("Would recommend buying the book");
+    var para = document.createElement("p");
+    para.appendChild(node);
+    para.style.color = "green";
+    var date = document.createTextNode(review.get("date"));
+    var datePar = document.createElement("p");
+    datePar.appendChild(date);
+    var description = document.createElement("p");
+    var desText = document.createTextNode(review.get("review"));
+    description.appendChild(desText);
+    element.appendChild(para);
+    element.appendChild(datePar);
+    element.appendChild(description);
+    var line = document.createElement("hr");
+    element.appendChild(line);
+  } else {
+    var node = document.createTextNode("Would NOT recommend buying the book");
+    var para = document.createElement("p");
+    para.appendChild(node);
+    para.style.color = "red";
+    var date = document.createTextNode(review.get("date"));
+    var datePar = document.createElement("p");
+    datePar.appendChild(date);
+    var description = document.createElement("p");
+    var desText = document.createTextNode(review.get("review"));
+    description.appendChild(desText);
+    element.appendChild(para);
+    element.appendChild(datePar);
+    element.appendChild(description);
+    var line = document.createElement("hr");
+    element.appendChild(line);
+  }
 }
 
 function loadResults(){
